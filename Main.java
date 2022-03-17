1147dfb032e3c1ef70ff4954f080cf306bbec83c
selva ganapathy <70571211+selvaganapathy-16@users.noreply.github.com>
2022-03-17 11:36:03 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main (String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter");
 		int n = sc.nextInt();
 		int i=0;
 		LinkedHashMap<String,Float> hm = new LinkedHashMap<String,Float>();
